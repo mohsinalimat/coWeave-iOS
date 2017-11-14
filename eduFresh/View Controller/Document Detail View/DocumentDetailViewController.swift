3826f5d0c87ad91d48fccf2bbe2d96b7bfa5e350
Benoît Frisch <benoit@fresh.lu>
2017-11-15 00:48:01 +0100

    Add Documents and DocumentsDetailView ViewController.

---
+++ eduFresh/View Controller/Document Detail View/DocumentDetailViewController.swift
@@ -0,0 +1,78 @@
+//
+//  DocumentDetailViewController.swift
+//  eduFresh
+//
+//  Created by Benoît Frisch on 15/11/2017.
+//  Copyright © 2017 Benoît Frisch. All rights reserved.
+//
+
+import UIKit
+
+class DocumentDetailViewController: UIViewController {
+    /**
+     *  Center Buttons, to add new data.
+     */
+    @IBOutlet var photoButton: UIButton!
+    @IBOutlet var videoButton: UIButton!
+    @IBOutlet var audioButton: UIButton!
+    @IBOutlet var drawButton: UIButton!
+    @IBOutlet var textButton: UIButton!
+    /**
+     *  Toolbar Buttons
+     */
+    @IBOutlet var previousPageButton: UIBarButtonItem!
+    @IBOutlet var nextPageButton: UIBarButtonItem!
+    @IBOutlet var pageNameButton: UIBarButtonItem!
+    @IBOutlet var pagesListButton: UIBarButtonItem!
+    /**
+     *  Navigation Bar Buttons
+     */
+    @IBOutlet var previewButton: UIBarButtonItem!
+    @IBOutlet var settingsButton: UIBarButtonItem!
+    /**
+     *
+     */
+    @IBOutlet var backgroundImageView: UIImageView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func photoAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func videoAction(_ sender: Any) {
+    }
+    
+    @IBAction func audioAction(_ sender: Any) {
+    }
+    
+    @IBAction func drawAction(_ sender: Any) {
+    }
+    
+    @IBAction func textAction(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ eduFresh/View Controller/RootTabBarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RootTabBarViewController.swift
+//  eduFresh
+//
+//  Created by Benoît Frisch on 15/11/2017.
+//  Copyright © 2017 Benoît Frisch. All rights reserved.
+//
+
+import UIKit
+
+class RootTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
