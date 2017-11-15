201b8a686e56c002e82132e31708280e9c435752
Benoît Frisch <benoit@fresh.lu>
2017-11-15 01:15:21 +0100

    Add Image Picker

--- Podfile
+++ Podfile
@@ -1,6 +1,6 @@
-platform :ios, '10.0'
+platform :ios, '11.0'
 use_frameworks!
 
 target 'eduFresh' do
-    pod 'Fusuma'
+    pod 'Fusuma', :git => 'https://github.com/ytakzk/Fusuma', :branch => 'swift4'
 end
--- eduFresh/View Controller/Document Detail View/DocumentDetailViewController.swift
+++ eduFresh/View Controller/Document Detail View/DocumentDetailViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import Fusuma
 
-class DocumentDetailViewController: UIViewController {
+class DocumentDetailViewController: UIViewController, FusumaDelegate {
     /**
      *  Center Buttons, to add new data.
      */
@@ -49,7 +50,11 @@ class DocumentDetailViewController: UIViewController {
     
 
     @IBAction func photoAction(_ sender: Any) {
-        
+        let fusuma = FusumaViewController()
+        fusuma.delegate = self
+        fusuma.cropHeightRatio = 0.6 // Height-to-width ratio. The default value is 1, which means a squared-size photo.
+        fusuma.allowMultipleSelection = false // You can select multiple photos from the camera roll. The default value is false.
+        self.present(fusuma, animated: true, completion: nil)
     }
     
     @IBAction func videoAction(_ sender: Any) {
@@ -63,6 +68,39 @@ class DocumentDetailViewController: UIViewController {
     
     @IBAction func textAction(_ sender: Any) {
     }
+    
+    
+    // Return the image which is selected from camera roll or is taken via the camera.
+    func fusumaImageSelected(_ image: UIImage, source: FusumaMode) {
+        self.backgroundImageView.image = image;
+        videoButton.isEnabled = false;
+        print("Image selected")
+    }
+    
+    // Return the image but called after is dismissed.
+    func fusumaDismissedWithImage(image: UIImage, source: FusumaMode) {
+        
+        print("Called just after FusumaViewController is dismissed.")
+    }
+    
+    func fusumaVideoCompleted(withFileURL fileURL: URL) {
+        
+        print("Called just after a video has been selected.")
+    }
+    
+    // When camera roll is not authorized, this method is called.
+    func fusumaCameraRollUnauthorized() {
+        
+        print("Camera roll unauthorized")
+    }
+    
+    // Return an image and the detailed information.
+    func fusumaImageSelected(_ image: UIImage, source: FusumaMode, metaData: ImageMetadata) {
+    }
+    
+    func fusumaMultipleImageSelected(_ images: [UIImage], source: FusumaMode) {
+    }
+    
     
     
     /*
