d447218fb33f2f1ce790a63e063d5a76e899c96c
Benoît Frisch <benoit@fresh.lu>
2017-11-17 14:42:51 +0100

    Add Audio Recorder

--- coWeave/View Controller/Document Detail View/DocumentDetailViewController.swift
+++ coWeave/View Controller/Document Detail View/DocumentDetailViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import iOSPhotoEditor
 import MobileCoreServices
 import CoreData
+import AVFoundation
 
 class DocumentDetailViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, PhotoEditorDelegate {
     var image : UIImage!
@@ -19,6 +20,15 @@ class DocumentDetailViewController: UIViewController, UINavigationControllerDele
     var pageImage : Image! = nil
     var pageNumber: Int16 = 1
     var managedObjectContext: NSManagedObjectContext!
+    /**
+     *  Audio Recorder
+     */
+    var recorder: AVAudioRecorder!
+    var player:AVAudioPlayer!
+    var meterTimer:Timer!
+    var soundFileURL:URL!
+    var audio : Bool = false
+    var playing : Bool = false
     /**
      *  Center Buttons, to add new data.
      */
@@ -92,6 +102,286 @@ class DocumentDetailViewController: UIViewController, UINavigationControllerDele
     }
     
     @IBAction func audioAction(_ sender: Any) {
+        print("audio")
+        if audio && !playing { // if sound recorded, play it.
+            startPlay()
+            return
+        }
+        if playing {
+            print("stopping")
+            stopPlay()
+            return
+        }
+        if (!audio) {
+            if recorder == nil {
+                print("recording. recorder nil")
+                self.audioButton.setImage(UIImage(named: "stop"), for: .normal)
+        
+                recordWithPermission(true)
+                return
+            }
+        }
+        if recorder != nil && recorder.isRecording {
+            print("\(#function)")
+        
+            recorder?.stop()
+            player?.stop()
+            
+            meterTimer.invalidate()
+            
+            let session = AVAudioSession.sharedInstance()
+            do {
+                try session.setActive(false)
+            } catch {
+                print("could not make session inactive")
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    
+    func startPlay() {
+        play()
+        self.audioButton.setImage(UIImage(named: "stop"), for: .normal)
+        playing = true
+    }
+    
+    func stopPlay() {
+        player.stop()
+        self.audioButton.setImage(UIImage(named: "play"), for: .normal)
+        playing = false
+    }
+    
+    
+    func updateAudioMeter(_ timer:Timer) {
+        if let recorder = self.recorder {
+            if recorder.isRecording {
+                let min = Int(recorder.currentTime / 60)
+                let sec = Int(recorder.currentTime.truncatingRemainder(dividingBy: 60))
+                let s = String(format: "%02d:%02d", min, sec)
+                self.audioButton.setTitle(s, for: .normal)
+                recorder.updateMeters()
+            }
+        }
+    }
+    
+    func play() {
+        print("\(#function)")
+        var url:URL?
+        if self.recorder != nil {
+            url = self.recorder.url
+        } else {
+            url = self.soundFileURL!
+        }
+        print("playing \(String(describing: url))")
+        
+        do {
+            self.player = try AVAudioPlayer(contentsOf: url!)
+            self.audioButton.imageView?.image = UIImage(named: "stop")
+            player.delegate = self
+            player.prepareToPlay()
+            player.volume = 1.0
+            player.play()
+        } catch {
+            self.player = nil
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    
+    func setupRecorder() {
+        print("\(#function)")
+        
+        let format = DateFormatter()
+        format.dateFormat="yyyy-MM-dd-HH-mm-ss"
+        let currentFileName = "recording-\(format.string(from: Date())).m4a"
+        print(currentFileName)
+        
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        self.soundFileURL = documentsDirectory.appendingPathComponent(currentFileName)
+        print("writing to soundfile url: '\(soundFileURL!)'")
+        
+        if FileManager.default.fileExists(atPath: soundFileURL.absoluteString) {
+            // probably won't happen. want to do something about it?
+            print("soundfile \(soundFileURL.absoluteString) exists")
+        }
+        
+        let recordSettings:[String : Any] = [
+            AVFormatIDKey:             kAudioFormatAppleLossless,
+            AVEncoderAudioQualityKey: AVAudioQuality.max.rawValue,
+            AVEncoderBitRateKey :      32000,
+            AVNumberOfChannelsKey:     2,
+            AVSampleRateKey :          44100.0
+        ]
+        
+        
+        do {
+            recorder = try AVAudioRecorder(url: soundFileURL, settings: recordSettings)
+            recorder.delegate = self
+            recorder.isMeteringEnabled = true
+            recorder.prepareToRecord() // creates/overwrites the file at soundFileURL
+        } catch {
+            recorder = nil
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    func recordWithPermission(_ setup:Bool) {
+        print("\(#function)")
+        
+        AVAudioSession.sharedInstance().requestRecordPermission() {
+            [unowned self] granted in
+            if granted {
+                
+                DispatchQueue.main.async {
+                    print("Permission to record granted")
+                    self.setSessionPlayAndRecord()
+                    if setup {
+                        self.setupRecorder()
+                    }
+                    self.recorder.record()
+                    
+                    self.meterTimer = Timer.scheduledTimer(timeInterval: 0.1,
+                                                           target:self,
+                                                           selector:#selector(self.updateAudioMeter(_:)),
+                                                           userInfo:nil,
+                                                           repeats:true)
+                }
+            } else {
+                print("Permission to record not granted")
+            }
+        }
+        
+        if AVAudioSession.sharedInstance().recordPermission() == .denied {
+            print("permission denied")
+        }
+    }
+    
+    func setSessionPlayback() {
+        print("\(#function)")
+        
+        let session = AVAudioSession.sharedInstance()
+        
+        do {
+            try session.setCategory(AVAudioSessionCategoryPlayback, with: .defaultToSpeaker)
+            
+        } catch {
+            print("could not set session category")
+            print(error.localizedDescription)
+        }
+        
+        do {
+            try session.setActive(true)
+        } catch {
+            print("could not make session active")
+            print(error.localizedDescription)
+        }
+    }
+    
+    func setSessionPlayAndRecord() {
+        print("\(#function)")
+        
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord, with: .defaultToSpeaker)
+        } catch {
+            print("could not set session category")
+            print(error.localizedDescription)
+        }
+        
+        do {
+            try session.setActive(true)
+        } catch {
+            print("could not make session active")
+            print(error.localizedDescription)
+        }
+    }
+    
+    func askForNotifications() {
+        print("\(#function)")
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector:#selector(DocumentDetailViewController.background(_:)),
+                                               name:NSNotification.Name.UIApplicationWillResignActive,
+                                               object:nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector:#selector(DocumentDetailViewController.foreground(_:)),
+                                               name:NSNotification.Name.UIApplicationWillEnterForeground,
+                                               object:nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector:#selector(DocumentDetailViewController.routeChange(_:)),
+                                               name:NSNotification.Name.AVAudioSessionRouteChange,
+                                               object:nil)
+    }
+    
+    func background(_ notification:Notification) {
+        print("\(#function)")
+        
+    }
+    
+    func foreground(_ notification:Notification) {
+        print("\(#function)")
+        
+    }
+    
+    
+    func routeChange(_ notification:Notification) {
+        print("\(#function)")
+        
+        if let userInfo = (notification as NSNotification).userInfo {
+            print("routeChange \(userInfo)")
+            
+            //print("userInfo \(userInfo)")
+            if let reason = userInfo[AVAudioSessionRouteChangeReasonKey] as? UInt {
+                //print("reason \(reason)")
+                switch AVAudioSessionRouteChangeReason(rawValue: reason)! {
+                case AVAudioSessionRouteChangeReason.newDeviceAvailable:
+                    print("NewDeviceAvailable")
+                    print("did you plug in headphones?")
+                    checkHeadphones()
+                case AVAudioSessionRouteChangeReason.oldDeviceUnavailable:
+                    print("OldDeviceUnavailable")
+                    print("did you unplug headphones?")
+                    checkHeadphones()
+                case AVAudioSessionRouteChangeReason.categoryChange:
+                    print("CategoryChange")
+                case AVAudioSessionRouteChangeReason.override:
+                    print("Override")
+                case AVAudioSessionRouteChangeReason.wakeFromSleep:
+                    print("WakeFromSleep")
+                case AVAudioSessionRouteChangeReason.unknown:
+                    print("Unknown")
+                case AVAudioSessionRouteChangeReason.noSuitableRouteForCategory:
+                    print("NoSuitableRouteForCategory")
+                case AVAudioSessionRouteChangeReason.routeConfigurationChange:
+                    print("RouteConfigurationChange")
+                    
+                }
+            }
+        }
+    }
+    
+    func checkHeadphones() {
+        print("\(#function)")
+        
+        // check NewDeviceAvailable and OldDeviceUnavailable for them being plugged in/unplugged
+        let currentRoute = AVAudioSession.sharedInstance().currentRoute
+        if currentRoute.outputs.count > 0 {
+            for description in currentRoute.outputs {
+                if description.portType == AVAudioSessionPortHeadphones {
+                    print("headphones are plugged in")
+                    break
+                } else {
+                    print("headphones are unplugged")
+                }
+            }
+        } else {
+            print("checking headphones requires a connection to a device")
+        }
     }
     
     @IBAction func drawAction(_ sender: Any) {
@@ -166,6 +456,8 @@ class DocumentDetailViewController: UIViewController, UINavigationControllerDele
         self.nextPageButton.isEnabled = true // always enabled, because we can add as many pages as we want
         self.nextPageButton.image = (page.next == nil) ? UIImage(named: "right-add") : UIImage(named: "right")
         self.pageNameButton.title = "Page \(page.number)"
+        
+        self.audioButton.imageView?.image = (page.audio == nil) ? UIImage(named: "micro") : UIImage(named: "play")
     }
     
     func updatePage(page: Page) {
@@ -361,5 +653,78 @@ public extension UIImage {
         
         guard let cgImage = image?.cgImage else { return nil }
         self.init(cgImage: cgImage)
+    }
+}
+
+
+
+// MARK: AVAudioRecorderDelegate
+extension DocumentDetailViewController : AVAudioRecorderDelegate {
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder,
+                                         successfully flag: Bool) {
+        
+        print("\(#function)")
+        
+        print("finished recording \(flag)")
+        
+        //recordButton.setTitle("Record", for:UIControlState())
+        
+        // iOS8 and later
+        let alert = UIAlertController(title: "Recorder",
+                                      message: "Finished Recording",
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Play", style: .default, handler: {action in
+            self.play()
+        }))
+        alert.addAction(UIAlertAction(title: "Keep", style: .default, handler: {action in
+            print("keep was tapped")
+            self.recorder = nil
+            self.audio = true
+            
+            self.audioButton.setImage(UIImage(named: "play"), for: .normal)
+            self.audioButton.setTitle("", for: .normal)
+        }))
+        alert.addAction(UIAlertAction(title: "Delete", style: .default, handler: {action in
+            print("delete was tapped")
+            self.recorder.deleteRecording()
+            self.audio = false
+            self.recorder = nil
+            self.soundFileURL = nil
+            
+            self.audioButton.setImage(UIImage(named: "micro"), for: .normal)
+            self.audioButton.setTitle("", for: .normal)
+        }))
+        self.present(alert, animated:true, completion:nil)
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder,
+                                          error: Error?) {
+        print("\(#function)")
+        
+        if let e = error {
+            print("\(e.localizedDescription)")
+        }
+    }
+    
+}
+
+// MARK: AVAudioPlayerDelegate
+extension DocumentDetailViewController : AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        print("\(#function)")
+        
+        print("finished playing \(flag)")
+        self.audioButton.setImage(UIImage(named: "play"), for: .normal)
+        self.audioButton.setTitle("", for: .normal)
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        print("\(#function)")
+        
+        if let e = error {
+            print("\(e.localizedDescription)")
+        }
+        
     }
 }
