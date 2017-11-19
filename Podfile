13c3201edba9beda319fd871f65fc12e39c677b5
Benoît Frisch <benoit@fresh.lu>
2017-11-19 01:46:48 +0100

    Add language switcher

--- Podfile
+++ Podfile
@@ -4,4 +4,5 @@ use_frameworks!
 target 'coWeave' do
     pod 'iOSPhotoEditor'
     pod 'Firebase/Core'
+    pod 'Localize-Swift', '~> 2.0'
 end
--- Pods/Target Support Files/Pods-coWeave/Pods-coWeave-frameworks.sh
+++ Pods/Target Support Files/Pods-coWeave/Pods-coWeave-frameworks.sh
@@ -103,11 +103,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Localize-Swift/Localize_Swift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/iOSPhotoEditor/iOSPhotoEditor.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Localize-Swift/Localize_Swift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/iOSPhotoEditor/iOSPhotoEditor.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
